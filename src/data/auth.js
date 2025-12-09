dfa9c3038ab6a68da8ca38d592187ea4771321c5
Himanshu <8967himanshusingh@gmail>
2025-12-09 15:45:48 +0530

    chnage the base url

--- src/data/auth.js
+++ src/data/auth.js
@@ -1,8 +1,8 @@
 // Authentication API service
-// const API_BASE =
-//   process.env.NEXT_PUBLIC_API_BASE || "https://rewardsapi.hireagent.co/api";
+const API_BASE =
+  process.env.NEXT_PUBLIC_API_BASE || "https://rewardsapi.hireagent.co/api";
 
-const API_BASE = "http://localhost:4001/api";
+// const API_BASE = "http://localhost:4001/api";
 
 export const authAPI = {
   // Admin login
